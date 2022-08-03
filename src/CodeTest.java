1574f30ca48ffd180b53d6ed8abb2f78b44b29ae
Leo Fabricio <leofabr3@gmail.com>
2022-08-03 17:25:03 -0300

    first commit

---
+++ src/CodeTest.java
@@ -0,0 +1,22 @@
+public class CodeTest {
+  public static void main(String[] args){
+
+
+    // Case Study: Financial Software
+
+    /**
+     * We have Stocks, Dividends Stocks, Mutual Funds, Real State, Cash....
+     *
+     *
+     * How you would design a complete portfolio system?
+     * What new types of objects would you write?
+     *
+     * **/
+
+    // 1st step - First we would have the classes Stocks, Dividend Stocks, Mutual Funds, Cash
+    // 2nd step - Relations : Dividend Stocs extends Stocks
+
+    // 3rd step -Because different assets compute their market values in different ways
+    //           We use Interface : getMarketValue and getProfit
+  }
+}
---
+++ src/DividendStocks.java
@@ -0,0 +1,2 @@
+public class DividendStocks extends Stocks{
+}
