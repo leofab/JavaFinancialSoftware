e85ed0be097bd77096e2909e2b3048aec706f3a3
Leo Fabricio <leofabr3@gmail.com>
2022-08-04 11:51:21 -0300

    stocks class methods implementation

--- src/Stocks.java
+++ src/Stocks.java
@@ -1,17 +1,35 @@
 public class Stocks extends SharedAsset implements Asset{
 
+  private int totalShares;
 
   public Stocks(String symbol, double totalCost, double currentPrice) {
     super(symbol, totalCost, currentPrice);
+    totalShares =0;
   }
+
 
   @Override
   public double getMarketValue() {
-    return 0;
+    return totalShares*getCurrentPrice();
   }
 
   @Override
   public double getProfit() {
-    return 0;
+    return getMarketValue() - getTotalCost();
+  }
+
+  public int getTotalShares() {
+    return totalShares;
+  }
+
+  public void setTotalShares(int totalShares) {
+    this.totalShares = totalShares;
+  }
+
+  // Records a purchase of the given number of shares of the
+  // stock at the given price per share
+  public void purchase(int shares, double pricePerShare){
+    totalShares += shares;
+    addCost(shares * pricePerShare);
   }
 }
