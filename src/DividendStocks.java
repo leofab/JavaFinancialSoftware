de4894f4fa7a95ee59f94151be424968fadbad32
Leo Fabricio <leofabr3@gmail.com>
2022-08-04 12:00:59 -0300

    implements mutual funds methods

--- src/DividendStocks.java
+++ src/DividendStocks.java
@@ -1,16 +1,24 @@
 public class DividendStocks extends Stocks{
 
+  private double dividends; // number of dividends paid
+
   public DividendStocks(String symbol, double totalCost, double currentPrice) {
     super(symbol, totalCost, currentPrice);
+    dividends = 0.0;
   }
 
   @Override
   public double getMarketValue() {
-    return super.getMarketValue();
+    return super.getMarketValue() + dividends;
   }
 
   @Override
   public double getProfit() {
     return super.getProfit();
+  }
+
+  // records a dividend of the given amount per share
+  public void payDividend(double amountPerShare){
+    dividends = amountPerShare * getTotalShares();
   }
 }
--- src/MutualFunds.java
+++ src/MutualFunds.java
@@ -1,11 +1,28 @@
-public class MutualFunds implements Asset{
+public class MutualFunds extends SharedAsset implements Asset{
+
+  private double totalShares;
+  public MutualFunds(String symbol, double totalCost, double currentPrice) {
+    super(symbol, totalCost, currentPrice);
+    totalShares = 0.0;
+  }
+
   @Override
   public double getMarketValue() {
-    return 0;
+    return totalShares * getCurrentPrice();
+  }
+
+  public double getTotalShares(){
+    return totalShares;
   }
 
   @Override
   public double getProfit() {
-    return 0;
+    return getMarketValue() - getTotalCost();
+  }
+
+  // records purchase of the given shares at the given market price
+  public void purchase(int shares, double pricePerShare){
+    totalShares += shares;
+    addCost(shares * pricePerShare);
   }
 }
